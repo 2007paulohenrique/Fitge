f679af616abd9b5f2ef1d28218239ea2bbe1ae9f
2007paulohenrique <pereirapaulo436@gmail.com>
2025-10-14 15:43:13 -0300

    Criação do componente de layout de cartão para cartão base.

---
+++ client/src/components/layout/cards/BaseCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import FlexContainer from '../containers/FlexContainer'
+
+type BaseCardProps = React.ComponentProps<typeof FlexContainer> & {
+    border?: string
+    borderRadius?: string | number 
+    boxShadow?: string
+}
+
+const BaseCard: React.FC<BaseCardProps> = ({
+    border = '2px solid var(--text-color)',
+    borderRadius = '20px',
+    boxShadow,
+    style,
+    children,
+    ...rest
+}) => {
+    return (
+        <FlexContainer
+            {...rest}
+            style={{
+                border,
+                borderRadius,
+                boxShadow,
+                overflow: 'hidden',
+                ...style
+            }}
+        >
+            {children}
+        </FlexContainer>
+    )
+}
+
+export default BaseCard
