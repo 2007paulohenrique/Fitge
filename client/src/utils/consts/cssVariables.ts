ed32d9caddc5b0089abbabe81231c4adae2ea8c0
2007paulohenrique <pereirapaulo436@gmail.com>
2025-10-15 17:46:28 -0300

    Criação das contantes de estilo global para a utilização nos arquivos em typescript

---
+++ client/src/utils/consts/cssVariables.ts
@@ -0,0 +1,18 @@
+export const themeColor = '#0056e8'
+export const lightThemeColor = '#5da8ff'
+export const darkThemeColor = '#002462'
+
+export const grayColor = '#bbbbbb'
+export const blackColor = '#000000'
+export const whiteColor = '#f5f5f5'
+
+export const alertColor = '#ff0000'
+export const successColor = '#5bf458'
+
+export const largeTitleSize = '3.5rem'
+export const titleSize = '2.5rem'
+export const smallTitleSize = '1.625rem'
+
+export const largeTextSize = '1.125rem'
+export const textSize = '0.875rem'
+export const smallTextSize = '0.7rem'
