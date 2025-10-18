fca928d3cf3c5500b68f1b5e93f6ce0481dad267
2007paulohenrique <pereirapaulo436@gmail.com>
2025-10-18 17:19:22 -0300

    Importação do i18n no arquivo principal para o funcionamento da tradução. + Aplicação dos provedores do contexto de mensagens de sistema, da store do redux e do BrowserRoter para o funcionamento do roteamento no arquivo principal.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './i18n'
 import App from './App.tsx'
+import { Provider } from 'react-redux'
+import { store } from './app/store/store.ts'
+import { SystemMessageProvider } from './app/contexts/systemMessageContext/SystemMessageContext.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider 
+      store={store}
+    >
+      <SystemMessageProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SystemMessageProvider>
+    </Provider>
   </StrictMode>
 )
