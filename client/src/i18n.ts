9d6cc57d08e4560da20a005a6f4ffb0ea2a847b0
2007paulohenrique <pereirapaulo436@gmail.com>
2025-10-18 15:55:02 -0300

    Criação dos arquivos de configuração do i18next e ajuste no arquivo de configuração do app typescript para o carregamento dos arquivos de tradução.

---
+++ client/src/i18n.ts
@@ -0,0 +1,30 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import Backend from 'i18next-http-backend'
+
+i18n
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: {
+            'pt': ['pt-BR'],
+            'default': ['pt-BR']
+        },
+        supportedLngs: ['pt-BR', 'en-US', 'es-ES'],
+        ns: ['translation'],
+        defaultNS: 'translation',
+        debug: true,
+        interpolation: {
+            escapeValue: false
+        },
+        react: {
+            useSuspense: false
+        },
+        backend: {
+            loadPath: '/locales/{{lng}}/translation.json'
+        }
+    })
+
+export default i18n
