33b2c664fee2161b679b34cc7daca45193ce86ab
2007paulohenrique <pereirapaulo436@gmail.com>
2025-10-24 00:11:23 -0300

    Criação das rotas para as página de login e registro.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
+import LoginPage from './components/pages/login/LoginPage'
+import { loginRote, signUpRote } from './utils/consts/rotes'
+import SignUpPage from './components/pages/signUp/SignUpPage'
+
 function App() {
   return (
-    <>
-    </>
+    <Routes>
+      <Route 
+        path={loginRote}
+        element={<LoginPage />} 
+      />
+      
+      <Route 
+        path={signUpRote}
+        element={<SignUpPage />} 
+      />
+    </Routes>
   )
 }
 
