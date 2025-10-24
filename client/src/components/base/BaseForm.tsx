b9e56cd22e693394f2334d7249f400a8844bca02
2007paulohenrique <pereirapaulo436@gmail.com>
2025-10-23 23:38:22 -0300

    Adição da lógica de aceitação dos termos ou políticas do aplicativo antes do envio de formulário no componente de base para formulários.

--- client/src/components/base/BaseForm.tsx
+++ client/src/components/base/BaseForm.tsx
@@ -4,18 +4,22 @@ import FlexContainer from '../containers/FlexContainer'
 type BaseFormProps = {
     onSubmit: () => void
     hasError: boolean
+    acceptTerms?: boolean
     children?: React.ReactNode
 }
 
 const BaseForm: React.FC<BaseFormProps> = ({
     onSubmit,
     hasError,
+    acceptTerms,
     children
 }) => {
     const handleOnError = useCallback((e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
 
         if (hasError) return
+
+        if (acceptTerms !== undefined && !acceptTerms) return
 
         onSubmit()
     }, [
