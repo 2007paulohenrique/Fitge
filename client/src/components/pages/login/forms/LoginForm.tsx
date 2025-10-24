d716bdfccf85dfc85b495a1df194c3b0f144b4dc
2007paulohenrique <pereirapaulo436@gmail.com>
2025-10-24 00:05:57 -0300

    Criação do componente do formulário para a página de login.

---
+++ client/src/components/pages/login/forms/LoginForm.tsx
@@ -0,0 +1,118 @@
+import { useTranslation } from 'react-i18next'
+import BaseForm from '../../../base/BaseForm'
+import FlexContainer from '../../../containers/FlexContainer'
+import Title from '../../../layout/text/Title'
+import PrimaryButton from '../../../layout/buttons/PrimaryButton'
+import TextButton from '../../../layout/buttons/TextButton'
+import { setObjectDataInForm } from '../../../../utils/reactState.ts/setData'
+import type { LoginUser } from '../types'
+import PasswordInput from '../../../layout/formFields/common/PasswordInput'
+import EmailInput from '../../../layout/formFields/specific/EmailInput'
+import { useNavigate } from 'react-router-dom'
+import { signUpRote } from '../../../../utils/consts/rotes'
+import FitgeIcon from '../../../../assets/fitgeIdentity/icon.svg?react'
+import IconIllustration from '../../../layout/illustrations/IconIllustration'
+
+type LoginFormProps = {
+    onLoginSubmit: () => void
+    loginError: boolean
+    setLoginError: React.Dispatch<React.SetStateAction<boolean>>
+    user: LoginUser
+    setUser: React.Dispatch<React.SetStateAction<LoginUser>>
+}
+
+const LoginForm: React.FC<LoginFormProps> = ({
+    onLoginSubmit,
+    loginError,
+    setLoginError,
+    user,
+    setUser
+}) => {
+    const { t } = useTranslation()
+
+    const navigate = useNavigate()
+
+    return (
+        <FlexContainer
+            gap='5em'
+        >
+            <FlexContainer
+                gap={0}
+            >
+                <IconIllustration
+                    icon={FitgeIcon}
+                />
+
+                <Title
+                    text={t('pages.login')}
+                />
+            </FlexContainer>
+
+            <BaseForm
+                onSubmit={onLoginSubmit}
+                hasError={loginError}
+            >
+                <FlexContainer
+                    gap='3em'
+                >
+                    <FlexContainer
+                        gap='0.5em'
+                    >
+                        <EmailInput
+                            value={user.email}
+                            onChange={(e) => setObjectDataInForm<LoginUser>(
+                                e,
+                                setUser,
+                                true,
+                                undefined,
+                                setLoginError
+                            )}
+                        />
+                        
+                        <PasswordInput
+                            name='password'
+                            placeholder={t('forms.fields.password.placeholder')}
+                            value={user.password}
+                            onChange={(e) => setObjectDataInForm<LoginUser>(
+                                e,
+                                setUser,
+                                true,
+                                undefined,
+                                setLoginError
+    
+                            )}
+                            label={t('forms.fields.password.label')}
+                            hasError={loginError}
+                            alertMessage={t('responses.error.LOGIN_ERROR')}
+                        />
+                    </FlexContainer>
+
+                    <FlexContainer
+                        gap='1.5em'
+                    >
+                        <PrimaryButton
+                            text={t('pages.login')}
+                        />
+
+                        <FlexContainer
+                            flexDirection='row'
+                        >
+                            <hr/>
+
+                            {t('general.or')}
+                            
+                            <hr/>
+                        </FlexContainer>
+
+                        <TextButton
+                            text={t('pages.signUp')}
+                            onClick={() => navigate(signUpRote)}
+                        />
+                    </FlexContainer>
+                </FlexContainer>
+            </BaseForm>
+        </FlexContainer>
+    )
+}
+
+export default LoginForm
