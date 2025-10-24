fd5082d1183ecf07a9252fa5a43eb3e623d0cc8a
2007paulohenrique <pereirapaulo436@gmail.com>
2025-10-24 02:34:32 -0300

    Criação do componente de layout para rodapé secundário do aplicativo.

---
+++ client/src/components/layout/footers/SecondaryFooter.tsx
@@ -0,0 +1,97 @@
+import { useNavigate } from "react-router-dom"
+import { aboutRoute, contentPolicyRoute, cookiesPolicyRoute, helpRoute, privacyAndSecurityPolicyRoute, termsAndConditionsRote } from "../../../utils/consts/rotes"
+import FlexContainer from "../../containers/FlexContainer"
+import FlexWrapContainer from "../../containers/FlexWrapContainer"
+import TextButton from "../../ui/buttons/TextButton"
+import { useTranslation } from "react-i18next"
+import type React from "react"
+import { formatVariableName } from "../../../utils/formatters/style/cssVariables"
+import { cssVarColors } from "../../../utils/consts/cssVariables"
+import useWindowSize from "../../../hooks/useWindowSize"
+
+const SecondaryFooter = () => {
+    const { t } = useTranslation()
+
+    const navigate = useNavigate()
+
+    const { isMobile } = useWindowSize()
+
+    type NavigateListElementProps = {
+        route: string
+        text: string
+    }
+
+    const NavigateListElement: React.FC<NavigateListElementProps> = ({
+        route,
+        text
+    }) => {
+        return (
+            <li>
+                <TextButton
+                    onClick={() => navigate(route)}
+                    text={text}
+                    varColor={cssVarColors.grayColor}
+                />
+            </li>
+        )
+    }
+
+    return (
+        <FlexContainer
+            tag='footer'
+        >
+            <FlexWrapContainer
+                tag="ul"
+                padding={isMobile ? '0 1em 4em' : '0 5em 4em'}
+                style={{
+                    color: formatVariableName(cssVarColors.grayColor),
+                    textAlign: 'center'
+                }}
+            >
+                <NavigateListElement
+                    route={aboutRoute}
+                    text={t('pages.about')}
+                />
+                
+                <NavigateListElement
+                    route={helpRoute}
+                    text={t('pages.help')}
+                />
+                
+                <NavigateListElement
+                    route={termsAndConditionsRote}
+                    text={t('pages.termsAndConditions')}
+                />
+                
+                <NavigateListElement
+                    route={privacyAndSecurityPolicyRoute}
+                    text={t('pages.privacyAndSecurityPolicy')}
+                />
+                
+                <NavigateListElement
+                    route={contentPolicyRoute}
+                    text={t('pages.contentPolicy')}
+                />
+                
+                <NavigateListElement
+                    route={cookiesPolicyRoute}
+                    text={t('pages.cookiesPolicy')}
+                />
+
+                <li>
+                    <TextButton
+                        onClick={() => undefined}
+                        text={t('general.download')}
+                        varColor={cssVarColors.grayColor}
+                    />
+                </li>
+                
+                <li>
+                    {t('identity.copyright')}
+                </li>
+            </FlexWrapContainer>
+        </FlexContainer>
+    )
+}
+
+export default SecondaryFooter
