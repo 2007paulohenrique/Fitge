61612783eeb864972a87ae2481291642c64a115b
2007paulohenrique <pereirapaulo436@gmail.com>
2025-10-24 02:33:29 -0300

    Criação das constantes das rotas das página de introdução do aplicativo, sobre, ajuda, política de privacidade e segurança, política de conteúdo e política de cookies.

--- client/src/utils/consts/rotes.ts
+++ client/src/utils/consts/rotes.ts
@@ -9,3 +9,15 @@ export const errorRote: string = '/error'
 export const termsAndConditionsRote: string = '/app/terms-and-conditions'
 
 export const policiesRote: string = '/app/policies'
+
+export const introductionRote: string = '/app/introduction'
+
+export const aboutRoute: string = '/app/about'
+
+export const helpRoute: string = '/app/help'
+
+export const privacyAndSecurityPolicyRoute: string = '/app/policies/privacy-and-security'
+
+export const contentPolicyRoute: string = '/app/policies/content'
+
+export const cookiesPolicyRoute: string = '/app/policies/cookies'
