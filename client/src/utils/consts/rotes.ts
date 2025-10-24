2b1403cc1320a2252ef673d34b79ef597faa06bc
2007paulohenrique <pereirapaulo436@gmail.com>
2025-10-23 23:10:02 -0300

    Criação das constantes das rotas de login, registro, recuperação de senha, erro, termos e condições, e políticas do aplicativo.

---
+++ client/src/utils/consts/rotes.ts
@@ -0,0 +1,11 @@
+export const loginRote: string = '/login'
+
+export const signUpRote: string = '/sign-up'
+
+export const recoverPasswordRote: string = '/recover-password'
+
+export const errorRote: string = '/error'
+
+export const termsAndConditionsRote: string = '/app/terms-and-conditions'
+
+export const policiesRote: string = '/app/policies'
