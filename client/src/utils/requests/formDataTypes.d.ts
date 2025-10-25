2ed8ba0d1c3c539ed9135349b042006d67e37941
2007paulohenrique <pereirapaulo436@gmail.com>
2025-10-25 15:30:55 -0300

    Criação dos tipos para os FormDatas enviados nas requisições para os end points da API para login, verificação dos dados ao criar conta, autenticação e confirmação de identidade.

---
+++ client/src/utils/requests/formDataTypes.d.ts
@@ -0,0 +1,28 @@
+import type { authEndPoint, identityConfirmationEndPoint, loginEndPoint, signUpEndPoint } from "../consts/apiEndPoints"
+
+export type SignUpFormData = {
+    name: string
+    nickname: string
+    email: string
+    password: string
+}
+
+export type LoginFormData = {
+    email: string
+    password: string
+}
+
+export type AuthFormData = {
+    secureId: string
+}
+
+export type IdentityConfirmationFormData = {
+    code: string
+}
+
+export type ApiFormDataMap = {
+    [signUpEndPoint]: SignUpFormData
+    [loginEndPoint]: LoginFormData
+    [authEndPoint]: AuthFormData
+    [identityConfirmationEndPoint]: IdentityConfirmationFormData
+}
