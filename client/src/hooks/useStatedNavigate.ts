49573edb10670c5bdd61f605ee13a2d85fe1b18a
2007paulohenrique <pereirapaulo436@gmail.com>
2025-10-25 18:05:35 -0300

    Ajuste no hook para navegação com estado tipado.

--- client/src/hooks/useStatedNavigate.ts
+++ client/src/hooks/useStatedNavigate.ts
@@ -1,16 +1,16 @@
 import { useNavigate, type To } from 'react-router-dom'
-import type { RouterStateMap } from '../utils/reactState.ts/routerStates';
+import type { RouterStateMap } from '../utils/reactState.ts/routerStates'
 
 type Exact<T> = T & Record<never, never>
 
 export default function useTypedNavigate() {
     const navigate = useNavigate()
 
-    function typedNavigate<T extends keyof RouterStateMap & To>(
+    function typedNavigate<T extends keyof RouterStateMap>(
         to: T,
         options: { state: Exact<RouterStateMap[T]>; replace?: boolean }
     ) {
-        navigate(to, options as any)
+        navigate(to as To, options as any)
     }
 
     return typedNavigate
