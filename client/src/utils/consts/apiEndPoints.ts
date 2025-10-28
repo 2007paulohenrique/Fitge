4a5aae1073fc77996af1c95d02a0babd968a8a0f
2007paulohenrique <pereirapaulo436@gmail.com>
2025-10-28 02:07:14 -0300

    Remoção do ";" dos arquivos das constantes para as rotas e para os end points da API.

--- client/src/utils/consts/apiEndPoints.ts
+++ client/src/utils/consts/apiEndPoints.ts
@@ -1,6 +1,6 @@
-export const GET_USER_ME_TYPE_ENDPOINT = '/users/me/type' as const;
-export const POST_REFRESH_TOKEN_ENDPOINT = '/token/refresh' as const;
-export const LOGIN_ENDPOINT = '/login' as const;
-export const SIGN_UP_ENDPOINT = '/sign-up' as const;
-export const AUTH_ENDPOINT = '/auth' as const;
-export const IDENTITY_CONFIRMATION_ENDPOINT = '/identity-confirmation' as const;
+export const GET_USER_ME_TYPE_ENDPOINT = '/users/me/type' as const
+export const POST_REFRESH_TOKEN_ENDPOINT = '/token/refresh' as const
+export const LOGIN_ENDPOINT = '/login' as const
+export const SIGN_UP_ENDPOINT = '/sign-up' as const
+export const AUTH_ENDPOINT = '/auth' as const
+export const IDENTITY_CONFIRMATION_ENDPOINT = '/identity-confirmation' as const
