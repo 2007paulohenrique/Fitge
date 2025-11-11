862db0a79efd4226cf033a3a7e3fd897f52faba9
2007paulohenrique <pereirapaulo436@gmail.com>
2025-11-11 15:04:47 -0300

    Ajuste nas expressões regulares para os dados do usuário para se adaptarem a mudança de tipo das expressões regulares básicas.

--- client/src/utils/constraints/userConstraints.ts
+++ client/src/utils/constraints/userConstraints.ts
@@ -2,26 +2,26 @@ import { HAS_ONLY_ALL_LETTER, HAS_ONLY_PERIOD, HAS_ONLY_SIMPLE_SPACE, HAS_ONLY_S
 
 export const NAME_MAX_LENGTH = 50
 export const NAME_REGEX = new RegExp(
-  `^[${HAS_ONLY_ALL_LETTER.source}${HAS_ONLY_PERIOD.source}${HAS_ONLY_SIMPLE_SPACE.source}]+$`
+  `^[${HAS_ONLY_ALL_LETTER}${HAS_ONLY_PERIOD}${HAS_ONLY_SIMPLE_SPACE}]+$`
 )
 
 export const NICKNAME_MIN_LENGTH = 3
 export const NICKNAME_MAX_LENGTH = 15
 export const NICKNAME_REGEX = new RegExp(
-  `^[${HAS_ONLY_ALL_LETTER.source}${HAS_ONLY_PERIOD.source}${HAS_ONLY_UNDERLINE.source}]+$`
+  `^[${HAS_ONLY_ALL_LETTER}${HAS_ONLY_PERIOD}${HAS_ONLY_UNDERLINE}${HAS_ONLY_NUMBER}]+$`
 )
 
 export const EMAIL_MAX_LENGTH = 254
 export const EMAIL_REGEX = new RegExp(
-  `^[${HAS_ONLY_SIMPLE_LETTER.source}${HAS_ONLY_NUMBER.source}._%+-]+@[` +
-  `${HAS_ONLY_SIMPLE_LETTER.source}${HAS_ONLY_NUMBER.source}.-]+\\.[` +
-  `${HAS_ONLY_SIMPLE_LETTER.source}]{2,}$`
+  `^[${HAS_ONLY_SIMPLE_LETTER}${HAS_ONLY_NUMBER}._%+-]+@[` +
+  `${HAS_ONLY_SIMPLE_LETTER}${HAS_ONLY_NUMBER}.-]+\\.[` +
+  `${HAS_ONLY_SIMPLE_LETTER}]{2,}$`
 )
 
 export const PASSWORD_MIN_LENGTH = 8
 export const PASSWORD_MAX_LENGTH = 20
 export const PASSWORD_REGEX = new RegExp(
-  `^${HAS_SIMPLE_LETTER.source}${HAS_NUMBER.source}${HAS_SYMBOL.source}[^\\s]+$`
+  `^${HAS_SIMPLE_LETTER}${HAS_NUMBER}${HAS_SYMBOL}[^\\s]+$`
 )
 
 export const MIN_AGE = 12
