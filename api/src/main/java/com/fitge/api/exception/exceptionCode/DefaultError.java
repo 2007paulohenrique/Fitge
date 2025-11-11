0339b16187d46e06e5c7d8bc453fcc5b95fa4016
2007paulohenrique <pereirapaulo436@gmail.com>
2025-11-11 17:50:17 -0300

    Criação da classe de erros padrão para erros genéricos no servidor, e na validação de idade, requisição e parâmetro de id na requisição na API..

---
+++ api/src/main/java/com/fitge/api/exception/exceptionCode/DefaultError.java
@@ -0,0 +1,9 @@
+package com.fitge.api.exception.exceptionCode;
+
+public class DefaultError {
+    public static final String INTERNAL_ERROR = "INTERNAL_ERROR"; 
+    public static final String MIN_AGE_DEFAULT_ERROR = "MIN_AGE_DEFAULT_ERROR"; 
+    public static final String INVALID_REQUEST = "INVALID_REQUEST"; 
+    public static final String INVALID_REQUEST_ID_PARAM = "INVALID_REQUEST_ID_PARAM"; 
+    
+}
