739b1b0642485d246c404f6e48bc8cc238308fe7
2007paulohenrique <pereirapaulo436@gmail.com>
2025-11-11 14:58:29 -0300

    Criação do tipo das possíveis rotas.

--- client/src/utils/consts/routes.ts
+++ client/src/utils/consts/routes.ts
@@ -12,3 +12,19 @@ export const HELP_ROUTE = '/app/help' as const
 export const PRIVACY_AND_SECURITY_POLICY_ROUTE = '/app/policies/privacy-and-security' as const
 export const CONTENT_POLICY_ROUTE = '/app/policies/content' as const
 export const COOKIES_POLICY_ROUTE = '/app/policies/cookies' as const
+
+export type routesType = 
+    typeof LOGIN_ROUTE |
+    typeof SIGN_UP_ROUTE |
+    typeof RECOVER_PASSWORD_ROUTE |
+    typeof IDENTITY_CONFIRMATION_ROUTE |
+    typeof SETUP_SETTINGS_ROUTE |
+    typeof ERROR_ROUTE |
+    typeof TERMS_AND_CONDITIONS_ROUTE |
+    typeof POLICIES_ROUTE |
+    typeof INTRODUCTION_ROUTE |
+    typeof ABOUT_ROUTE |
+    typeof HELP_ROUTE |
+    typeof PRIVACY_AND_SECURITY_POLICY_ROUTE |
+    typeof CONTENT_POLICY_ROUTE |
+    typeof COOKIES_POLICY_ROUTE
