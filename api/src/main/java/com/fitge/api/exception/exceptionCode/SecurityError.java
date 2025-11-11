5999090ff33e2eae71e88728e59abe80bdb1582b
2007paulohenrique <pereirapaulo436@gmail.com>
2025-11-11 17:56:13 -0300

    Criação da classe de erros para erros de segurança ocorridos na verificação do login, sign up, token de autenticação e tipo do usuário na API.

---
+++ api/src/main/java/com/fitge/api/exception/exceptionCode/SecurityError.java
@@ -0,0 +1,10 @@
+package com.fitge.api.exception.exceptionCode;
+
+public class SecurityError {
+    public static final String LOGIN_ERROR = "LOGIN_ERROR";   
+    public static final String SIGN_UP_ERROR = "SIGN_UP_ERROR";   
+    public static final String INVALID_TOKEN_ERROR = "INVALID_TOKEN_ERROR";   
+    public static final String REQUIRED_USER_ROLE_CLIENT_ERROR = "REQUIRED_USER_ROLE_CLIENT_ERROR";   
+    public static final String REQUIRED_USER_ROLE_TRAINER_ERROR = "REQUIRED_USER_ROLE_TRAINER_ERROR";   
+    public static final String INVALID_USER_ROLE_ERROR = "INVALID_USER_ROLE_ERROR";   
+}
