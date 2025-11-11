727e8740764d5cf37451e70bf16d7230c7bd4846
2007paulohenrique <pereirapaulo436@gmail.com>
2025-11-11 15:01:44 -0300

    Remoção das constantes das chaves de cache para token do login e da criação de conta + Alteração do tipo das expressões regulares de regex para string.

--- client/src/utils/constraints/regex.ts
+++ client/src/utils/constraints/regex.ts
@@ -1,13 +1,13 @@
-export const HAS_SIMPLE_LETTER = /(?=.*[A-Za-z])/
-export const HAS_NUMBER = /(?=.*\d)/
-export const HAS_SYMBOL = /(?=.*[!@#$%^&*()_+\-=\[\]{}':"\\|,.<>/?])/
+export const HAS_SIMPLE_LETTER = "(?=.*[A-Za-z])";
+export const HAS_NUMBER = "(?=.*\\d)";
+export const HAS_SYMBOL = "(?=.*[!@#$%^&*()_+\\-=\\[\\]{}':\"\\\\|,.<>/?])";
 
-export const HAS_NOT_LINE_BREAKS = /(?!.*(\r|\n))/
-export const HAS_NOT_DOUBLE_SPACES = /(?!.*(  ))/
+export const HAS_NOT_LINE_BREAKS = "(?!.*(\\r|\\n))";
+export const HAS_NOT_DOUBLE_SPACES = "(?!.*(  ))";
 
-export const HAS_ONLY_SIMPLE_LETTER = /[A-Za-z]/
-export const HAS_ONLY_NUMBER = /[0-9]/
-export const HAS_ONLY_ALL_LETTER = /[A-Za-zÀ-ÖØ-öø-ÿçÇ]/
-export const HAS_ONLY_SIMPLE_SPACE = / /
-export const HAS_ONLY_PERIOD = /\./
-export const HAS_ONLY_UNDERLINE = /_/
+export const HAS_ONLY_SIMPLE_LETTER = "A-Za-z";
+export const HAS_ONLY_NUMBER = "0-9";
+export const HAS_ONLY_ALL_LETTER = "A-Za-zÀ-ÖØ-öø-ÿçÇ";
+export const HAS_ONLY_SIMPLE_SPACE = " ";
+export const HAS_ONLY_PERIOD = "\\.";
+export const HAS_ONLY_UNDERLINE = "_";
--- client/src/utils/consts/cacheKeys.ts
+++
@@ -1,2 +0,0 @@
-export const CREATE_ACCOUNT_SECURE_ID_KEY = 'createAccountSecureId' as const
-export const LOGIN_SECURE_ID_KEY = 'logintSecureId' as const
