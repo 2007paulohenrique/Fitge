2fd36e9e4572763ce8a1a478b360a795af0dc21a
2007paulohenrique <pereirapaulo436@gmail.com>
2025-11-11 17:54:32 -0300

    Criação da classe de erros para erros ocorridos na criação de media e upload de arquivos na API devido ao formato e tamanho do arquivo.

---
+++ api/src/main/java/com/fitge/api/exception/exceptionCode/MediaError.java
@@ -0,0 +1,10 @@
+package com.fitge.api.exception.exceptionCode;
+
+public class MediaError {
+    public static final String UPLOAD_FILE_ERROR = "UPLOAD_FILE_ERROR"; 
+    public static final String MEDIA_NOT_FOUND = "MEDIA_NOT_FOUND"; 
+    public static final String MEDIA_MAX_SIZE_ERROR = "MEDIA_MAX_SIZE_ERROR"; 
+    public static final String MEDIA_TYPE_ERROR = "MEDIA_TYPE_ERROR"; 
+    public static final String EMPTY_MEDIA_FILE = "EMPTY_MEDIA_FILE"; 
+
+}
