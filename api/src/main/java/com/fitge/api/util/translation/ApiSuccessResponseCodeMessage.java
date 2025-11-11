7fbd727cf2c545961893e6e06f6d193ee36c22f2
2007paulohenrique <pereirapaulo436@gmail.com>
2025-11-11 17:26:19 -0300

    Criação do arquivo de utilidades para os códigos de tradução em caso de sucesso na requisição na API com os código de sucesso para recuperar e criar cliente, login e sign up.

---
+++ api/src/main/java/com/fitge/api/util/translation/ApiSuccessResponseCodeMessage.java
@@ -0,0 +1,8 @@
+package com.fitge.api.util.translation;
+
+public class ApiSuccessResponseCodeMessage {
+    public static final String GET_CLIENT_SUCCESS = "GET_CLIENT_SUCCESS";
+    public static final String POST_CLIENT_SUCCESS = "POST_CLIENT_SUCCESS";
+    public static final String POST_LOGIN_SUCCESS = "POST_LOGIN_SUCCESS";
+    public static final String POST_SIGN_UP_SUCCESS = "POST_SIGN_UP_SUCCESS";
+}
