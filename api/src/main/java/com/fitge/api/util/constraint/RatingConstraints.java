fabc355103c1c4b3e42df6ca212f03ec54a28f69
2007paulohenrique <pereirapaulo436@gmail.com>
2025-11-12 15:05:45 -0300

    Criação da classe com as constantes de validação dos dados da avaliação..

---
+++ api/src/main/java/com/fitge/api/util/constraint/RatingConstraints.java
@@ -0,0 +1,6 @@
+package com.fitge.api.util.constraint;
+
+public class RatingConstraints {
+    public static final Double RATING_MAX_VALUE_DOUBLE = 10.0;
+    public static final String RATING_MAX_VALUE_STRING = "" + 10.0;
+}
