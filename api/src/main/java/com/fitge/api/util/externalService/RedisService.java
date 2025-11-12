1613d7cfa525d57a0681b471959460ea24766b65
2007paulohenrique <pereirapaulo436@gmail.com>
2025-11-12 17:48:57 -0300

    Criação do serviço do redis na API com funções de salvamento de valor com e sem tempo de expiração, recuperação e remoção de valor.

---
+++ api/src/main/java/com/fitge/api/util/externalService/RedisService.java
@@ -0,0 +1,33 @@
+package com.fitge.api.util.externalService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+public class RedisService {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    public RedisService(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void saveValue(String key, Object value, long expirationMs) {
+        redisTemplate.opsForValue().set(key, value, Duration.ofMillis(expirationMs));
+    }
+
+    public void saveValueWithoutExpiration(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public Object getValue(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void deleteValue(String key) {
+        redisTemplate.delete(key);
+    }
+}
