acb94f7a6b892b7ade87703b81ad4b3904028947
2007paulohenrique <pereirapaulo436@gmail.com>
2025-11-12 17:42:25 -0300

    Criação da classe das constantes dos nomes da tabelas e suas respectivas colunas de chave estrangeira na API para as tabelas de: usuário, cliente, treinador, país, mídia, tipo da mídia, rede social, local de atendimento, rede social e habilidade do treinador, experiência e objetivo no treino, plano do aplicativo, compra do plano do aplicativo, compra, reembolso da compra, linguagem e auditoria de login.

---
+++ api/src/main/java/com/fitge/api/util/database/TableName.java
@@ -0,0 +1,41 @@
+package com.fitge.api.util.database;
+
+public class TableName {
+    public static final String USER_TABLE = "tb_user";
+    public static final String CLIENT_TABLE = "tb_client";
+    public static final String TRAINER_TABLE = "tb_trainer";
+    public static final String COUNTRY_TABLE = "tb_country";
+    public static final String MEDIA_TABLE = "tb_media";
+    public static final String MEDIA_TYPE_TABLE = "tb_media_type";
+    public static final String SOCIAL_NETWORK_TABLE = "tb_social_network";
+    public static final String TRAINER_LOCATION_TABLE = "tb_trainer_location";
+    public static final String TRAINER_SOCIAL_NETWORK_TABLE = "tb_trainer_social_network";
+    public static final String TRAINING_EXPERIENCE_TABLE = "tb_training_experience";
+    public static final String TRAINING_TARGET_TABLE = "tb_training_target";
+    public static final String LOGIN_AUDIT_TABLE = "tb_login_audit";
+    public static final String TRAINER_SKILL_TABLE = "tb_trainer_skill";
+    public static final String FITGE_PLAN_TABLE = "tb_fitge_plan";
+    public static final String FITGE_PLAN_PURCHASE_TABLE = "tb_fitge_plan_purchase";
+    public static final String PURCHASE_TABLE = "tb_purchase";
+    public static final String PURCHASE_REFUND_TABLE = "tb_purchase_refund";
+    public static final String LANGUAGE_TABLE = "tb_language";
+    
+    public static final String USER_FK_COLUMN = "fk_" + USER_TABLE + "_id";
+    public static final String CLIENT_FK_COLUMN = "fk_" + CLIENT_TABLE + "_id";
+    public static final String TRAINER_FK_COLUMN = "fk_" + TRAINER_TABLE + "_id";
+    public static final String COUNTRY_FK_COLUMN = "fk_" + COUNTRY_TABLE + "_id";
+    public static final String MEDIA_FK_COLUMN = "fk_" + MEDIA_TABLE + "_id";
+    public static final String MEDIA_TYPE_FK_COLUMN = "fk_" + MEDIA_TYPE_TABLE + "_id";
+    public static final String SOCIAL_NETWORK_FK_COLUMN = "fk_" + SOCIAL_NETWORK_TABLE + "_id";
+    public static final String TRAINER_LOCATION_FK_COLUMN = "fk_" + TRAINER_LOCATION_TABLE + "_id";
+    public static final String TRAINER_SOCIAL_NETWORK_FK_COLUMN = "fk_" + TRAINER_SOCIAL_NETWORK_TABLE + "_id";
+    public static final String TRAINING_EXPERIENCE_FK_COLUMN = "fk_" + TRAINING_EXPERIENCE_TABLE + "_id";
+    public static final String TRAINING_TARGET_FK_COLUMN = "fk_" + TRAINING_TARGET_TABLE + "_id";
+    public static final String LOGIN_AUDIT_FK_COLUMN = "fk_" + LOGIN_AUDIT_TABLE + "_id";
+    public static final String TRAINER_SKILL_FK_COLUMN = "fk_" + TRAINER_SKILL_TABLE + "_id";
+    public static final String FITGE_PLAN_FK_COLUMN = "fk_" + FITGE_PLAN_TABLE + "_id";
+    public static final String FITGE_PLAN_PURCHASE_FK_COLUMN = "fk_" + FITGE_PLAN_PURCHASE_TABLE + "_id";
+    public static final String PURCHASE_FK_COLUMN = "fk_" + PURCHASE_TABLE + "_id";
+    public static final String PURCHASE_REFUND_FK_COLUMN = "fk_" + PURCHASE_REFUND_TABLE + "_id";
+    public static final String LANGUAGE_FK_COLUMN = "fk_" + LANGUAGE_TABLE + "_id";
+}
