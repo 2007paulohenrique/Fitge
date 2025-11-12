b55f76613a1f55f39e55c79477db1d210c01b4c0
2007paulohenrique <pereirapaulo436@gmail.com>
2025-11-12 18:10:29 -0300

    Criação da classe de configuração de segurança na API para definição de controllers públicos e aplicação do filtro de autenticação com jwt, complementando a aplicação da configuração do cors e outras configurações de segurança da API.

---
+++ api/src/main/java/com/fitge/api/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.fitge.api.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fitge.api.util.constraint.ControllerConstraint;
+
+@EnableMethodSecurity
+@Configuration
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .cors(cors -> {})
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(ControllerConstraint.PUBLIC_CONTROLLERS.toArray(new String[0])).permitAll()
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
