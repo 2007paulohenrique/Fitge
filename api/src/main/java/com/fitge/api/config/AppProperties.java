62ba93aec95516a07fa79f560e8125a4a9079aca
2007paulohenrique <pereirapaulo436@gmail.com>
2025-11-12 14:14:47 -0300

    Criação da classe das propriedades do aplicativo spring boot na API.

---
+++ api/src/main/java/com/fitge/api/config/AppProperties.java
@@ -0,0 +1,22 @@
+package com.fitge.api.config;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "app")
+@Validated
+public class AppProperties {
+    @Min(1)
+    private int port;
+
+    @NotBlank
+    private String activeProfile;
+
+}
